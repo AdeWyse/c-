f70aefb7dd4895f883de9bb01cbcbaf0182281d8
AdeWYyse <adelinewyse@gmail.com>
2022-09-20 14:21:05 -0300

    Creation

---
+++ Primeiro/Primeiro.cpp
@@ -0,0 +1,15 @@
+// Primeiro.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include <iostream>
+
+int main()
+{
+    int age{}, born{}, now{};
+    std::cout << "Hello What year where you born? What year is now? \n";
+    std::cin >> born >> now;
+    age = now - born;
+    std::cout << "Your age is " << age << ". Bye!\n";
+    return 0;
+}
+
